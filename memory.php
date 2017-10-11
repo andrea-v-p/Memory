650241533fa092b7690edb3f2f958a01f2be7793
andrea-v-p <32142430+andrea-v-p@users.noreply.github.com>
2017-10-11 12:55:44 +0200

    Update memory.php

--- memory.php
+++ memory.php
@@ -10,8 +10,9 @@
 	$col = 2;
 	$fil = 2;
 	$cont =0;
-	$cartas2X2 = array( 1, 2, 1, 2);
-//	$cartas6X6 = array( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18);
+	// cartas4X4 array temporal falta añadir el resto de imagenes
+		// 16 CARTAS -- 8 PAREJAS
+	$cartas4X4 = array( 1, 2, 1, 2);
 	shuffle($numeros);
 	echo ("<table>");
 
@@ -19,7 +20,7 @@
 			echo ("<tr>");
 			for ($x=0; $x < $fil ; $x++) {
 				echo ("	<td><div class=\"container\">
-							<img src=\"imagenes/$cartas2X2[$cont].jpeg\" style=\"width:120px;height:180px;\" class=\"back\">
+							<img src=\"imagenes/$cartas4X4[$cont].jpeg\" style=\"width:120px;height:180px;\" class=\"back\">
 							<img src=\"imagenes/dorso.jpeg\" style=\"width:120px;height:180px;\" class=\"front\">
 						</div></td>");
 				$cont++;
@@ -30,10 +31,9 @@
 	echo ("</table>");
 ?>
 
-
-<button id="restart" onclick="inicio()">
-  ¡Reiniciar!
-</button>
+	<a href="inicio.html"><button id="restart" >
+ 		¡Reiniciar!
+	</button></a>
 
 </body>
 </html>
