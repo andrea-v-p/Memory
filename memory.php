6407c520af1398df346d6ccf3c0ba61da5cb1679
andrea-v-p <32142430+andrea-v-p@users.noreply.github.com>
2017-10-12 13:57:53 +0200

    Update memory.php

--- memory.php
+++ memory.php
@@ -15,26 +15,21 @@
 	$cartas4X4 = array( 1, 1, 2, 2);
 	shuffle($cartas4X4);
 	echo ("<table>");
-
 		for ($i=0; $i < $col ; $i++) { 
 			echo ("<tr>");
 			for ($x=0; $x < $fil ; $x++) {
 				echo ('	<td><div class="container" onclick=" flip('.$cont.')">
-							<img src="imagenes/'.$cartas4X4[$cont].'.png" style="width:120px;height:180px;" class="back" id="'.$cont.'f">
-							<img src="imagenes/dorso.png" style="width:120px;height:180px;" class="front" id="'.$cont.'b ">
+							<img src="imagenes/'.$cartas4X4[$cont].'.png" style="width:120px;height:180px;" class="front" id="'.$cont.'f">
+							<img src="imagenes/dorso.png" style="width:120px;height:180px;" class="back" id="'.$cont.'b">
 						</div></td>');
 				$cont++;
-
 			}
 			echo ("</tr>");
-		
 	}
 	echo ("</table>");
 ?>
 
-	<!-- <a href="inicio.html"> --><button id="restart" >
- 		¡Reiniciar!
-	</button><!-- </a> -->
+	<button id="restart">Reiniciar</button>
 
 </body>
 </html>
