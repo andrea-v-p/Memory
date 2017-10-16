f11a2be127ae37da9e2e916abdb67b4f476175df
andrea-v-p <avidalpaniagua@iesesteveterradas.cat>
2017-10-16 20:30:03 +0200

    falta contador tiempo

--- memory.js
+++ memory.js
@@ -1,13 +1,110 @@
-var intentos = 0;
+// COMPROBAR SI ES LA MISMA CARTA
 
-function flip($identificador){
-	var x = document.getElementById($identificador+"b");
 
-	if (x.className=="backFlip"){
-		document.getElementById($identificador+"b").className = "back";
-		document.getElementById($identificador+"f").className = "front";
-	}else if(x.className=="back"){
-		document.getElementById($identificador+"b").className = "backFlip";
-		document.getElementById($identificador+"f").className = "frontFlip";
+function click1($identificador, $totalCartas){
+	var intentos = 0;
+	var tCarta = $totalCartas;
+	var cards;
+	var identificador = $identificador;
+
+	if (cuantasCartas(tCarta) == 0){
+		flip(identificador);
+
+	}else if (cuantasCartas(tCarta) == 1){
+		flip(identificador);
+
+		cards = queCartas(tCarta);
+		idCards = queCartasId(tCarta);
+		intentos ++;
+
+		if (CompruebaIguales(idCards)==true) {
+
+			document.getElementById(idCards[0]+"b").className = "backFlip2";
+			document.getElementById(idCards[0]+"f").className = "frontFlip2";
+
+			document.getElementById(idCards[1]+"b").className = "backFlip2";
+			document.getElementById(idCards[1]+"f").className = "frontFlip2";
+
+			if(CompruebaFinal(tCarta)){
+				alert(intentos);
+			}
+		}else{
+			flip(idCards[0]);
+			flip(idCards[1]);
+		}
+	}
+}
+
+function cuantasCartas(cartas) {
+	var carta=0;
+	var id;
+	for (var i = 0; i < cartas; i++){
+		id = document.getElementById(i+"f");
+		if(id.className == "front"){
+			carta ++;
+		}
+	}
+	return carta;
+}
+
+function CompruebaIguales(cards) {
+	var id1 = document.getElementById(cards[0]+'f').src;
+	var id2 = document.getElementById(cards[1]+'f').src;
+	console.log(id1, id2);
+	if(id1 == id2){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+function CompruebaFinal(tCarta) {
+	var total;
+	for(var i = 0; i < tCarta; i++){
+		if (document.getElementById(i+"b").className == "backFlip2"){
+			total++;
+		}
+		if (total == tCarta){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
+
+function queCartas(tCarta) {
+	var cards = new Array();
+	var cartas = tCarta;
+	var id;
+	for (var i = 0; i < cartas; i++) {
+		id = document.getElementById(i+"f");
+		if(id.className == "front"){
+			cards.push(id);
+		}
+	}
+	return cards;
+}
+function queCartasId(tCarta) {
+	var cartasId = new Array();
+	var carta = tCarta
+	var id;
+	for (var y = 0; y < carta; y++) {
+		id = document.getElementById(y+"f");
+		if(id.className == "front"){
+			cartasId.push(y);
+		}
+	}
+	return cartasId;
+}
+
+function flip(identificador){
+	var x = document.getElementById(identificador+"f");
+
+	if (x.className=="frontFlip"){
+		document.getElementById(identificador+"b").className = "back";
+		document.getElementById(identificador+"f").className = "front";
+	}else if(x.className=="front"){
+		document.getElementById(identificador+"b").className = "backFlip";
+		document.getElementById(identificador+"f").className = "frontFlip";
 	}
 }
--- memory.php
+++ memory.php
@@ -8,19 +8,21 @@
 <body>
 <?php 
 	$col = 2;
-	$fil = 2;
+	$fil = 4;
+	$total = $col*$fil;
 	$cont =0;
 	// cartas4X4 array temporal falta añadir el resto de imagenes
 		// 16 CARTAS -- 8 PAREJAS
-	$cartas4X4 = array( 1, 1, 2, 2);
+			// 5, 6, 7, 8,  , 5, 6, 7, 8
+	$cartas4X4 = array( 1, 2, 3, 4, 1, 2, 3, 4);
 	shuffle($cartas4X4);
 	echo ("<table>");
 		for ($i=0; $i < $col ; $i++) { 
 			echo ("<tr>");
 			for ($x=0; $x < $fil ; $x++) {
-				echo ('	<td><div class="container" onclick=" flip('.$cont.')">
-							<img src="imagenes/'.$cartas4X4[$cont].'.png" style="width:120px;height:180px;" class="frontFlip" id="'.$cont.'f">
-							<img src="imagenes/dorso.png" style="width:120px;height:180px;" class="backFlip" id="'.$cont.'b">
+				echo ('	<td><div class="container" onclick="click1('.$cont.', '.$total.')">
+							<img src="Imagenes/'.$cartas4X4[$cont].'.png" style="width:120px;height:180px;" class="frontFlip" id="'.$cont.'f">
+							<img src="Imagenes/dorso.png" style="width:120px;height:180px;" class="backFlip" id="'.$cont.'b">
 						</div></td>');
 				$cont++;
 			}
